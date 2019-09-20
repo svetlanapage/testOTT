4b0f90950a46ec079f9035dc5b93c3df620b6c97
s.yureva <svetlana.page@gmail.com>
2019-09-20 14:37:08 +0300

    commit message

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -10,7 +10,7 @@ import io.restassured.matcher.RestAssuredMatchers;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+//import java.net.URL;
 
 
 public class ApiTest {
